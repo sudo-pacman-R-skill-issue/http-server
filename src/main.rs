f49990e011d671f6f61f83689de425fb32260082
sudo-pacman-R-skill-issue <iamtugarin@gmail.com>
2025-07-09 17:07:24 +0300

    ok

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,18 @@
-#[allow(unused_imports)]
 use std::net::TcpListener;
-
+mod second {
+    include!("../src_1/main.rs");
+}
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_stream) => {
+                println!("accepted new connection");
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
 
-    // Uncomment this block to pass the first stage
-    //
-    // let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    //
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(_stream) => {
-    //             println!("accepted new connection");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
 }
