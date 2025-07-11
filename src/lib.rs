a128d352182cb428c1c2fd001f6b4a8314c9de1e
sudo-pacman-R-skill-issue <iamtugarin@gmail.com>
2025-07-11 17:13:07 +0300

    feat: implement Request struct and main function for HTTP server

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,111 @@
-use std::net::TcpStream;
+use std::{collections::HashMap, io::{BufRead, BufReader, Bytes, Error, ErrorKind, Read, Write}, net::TcpStream};
+use bumpalo::Bump;
+use http_server_lib::HttpTemplate;
 
-#[derive(Debug)]
 pub struct Request<'a> {
     pub method: &'a str,
     pub path: &'a str,
     pub protocol: &'a str,
-    pub headers: &'a str,
-    pub body: &'a str,
+    pub headers: HashMap<&'a mut str, &'a mut str>,
+    pub body: Option<&'a str>,
 }
 
-pub struct Response<'a> {
-    pub method: &'a str,
-    pub path: &'a str,
-    pub protocol: &'a str,
-    pub headers: &'a str,
-    pub body: &'a str,
+impl<'a> Request<'a> {
+    fn new() -> Self {
+        Request {
+            method: "",
+            path: "",
+            protocol: "",
+            headers: HashMap::new(),
+            body: None,
+        }
+    }
+    
+    fn from(
+        method: &'a str,
+        path: &'a str,
+        protocol: &'a str,
+        headers: HashMap<&'a mut str, &'a mut str>,
+    ) -> Self {
+        Self {
+            method,
+            path,
+            protocol,
+            headers,
+            body: None,
+        }
+    }
+
+    pub fn from_stream(stream: &TcpStream, bump: &'a Bump) -> Result<Self, Error> {
+        let mut buf_reader = BufReader::new(stream);
+        let mut buf_line = String::new();
+        buf_reader.read_line(&mut buf_line)?;
+        let mut http = buf_line
+            .split_whitespace();
+        // dbg!(&http);
+        // dbg!(&buf_line);
+        let method = bump.alloc_str(http.next().ok_or_else(|| {
+            std::io::Error::new(ErrorKind::InvalidData, "missing method")
+        })?);
+        let path = bump.alloc_str(http.next().ok_or_else(|| {
+            std::io::Error::new(ErrorKind::InvalidData, "missing path")
+        })?);
+        let protocol = bump.alloc_str(http.next().ok_or_else(|| {
+            std::io::Error::new(ErrorKind::InvalidData, "missing protocol")
+        })?);
+        let headers = { 
+            http.next().ok_or("");
+            let mut headers = HashMap::new();
+            loop {
+                let mut lline = String::new();
+                let bytes = buf_reader.read_line(&mut lline).unwrap_or(0);
+                if bytes == 0 || lline == "\r\n" || lline == "\n" {
+                    break;
+                }
+                if let Some((key, value)) = lline.split_once(": ") {
+                    headers.insert(bump.alloc_str(key), bump.alloc_str(value));
+                }
+            }
+            headers
+        };
+        Ok(Request::from(method, path, protocol, headers))
+    }
 }
 
-pub fn req_handler(stream: &mut TcpStream) -> Response {
-    todo!()
+pub enum HttpTemplates {
+    PlainText,
+    OctetStream,
+    Json,
+    Created,
+    NotFound,
+    Slash,
+}
+
+impl HttpTemplates {
+    #[rustfmt::skip]
+    pub fn format(self, content: &str) -> Vec<u8> {
+        match self {
+            HttpTemplates::PlainText => {
+                format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                content.len(),
+                content).into_bytes()
+            },
+            HttpTemplates::OctetStream => {
+                format!("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length:{}\r\n\r\n{}",
+                content.len(),
+                content).into_bytes()
+            },
+            HttpTemplates::Json => {
+                format!("HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length:{}\r\n\r\n{}",
+                content.len(),
+                content).into_bytes()},
+            HttpTemplates::Created => {
+                b"HTTP/1.1 201 Created\r\n\r\n".to_vec()
+            },
+            HttpTemplates::Slash => {
+                b"HTTP/1.1 200 OK\r\n\r\n".to_vec()
+            }
+            HttpTemplates::NotFound => b"HTTP/1.1 404 Not Found\r\n\r\n".to_vec(),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,37 @@
+use std::{
+    convert::Infallible, io::{BufRead, BufReader, Error, ErrorKind, Read, Write}, net::{TcpListener, TcpStream}, ops::Index
+};
+use bumpalo::Bump;
+
+use crate::lib::{HttpTemplates, Request};
 mod lib;
-use std::net::TcpListener;
-
-use crate::lib::*;
-
+// use crate::lib::req_handler;
 mod second {
     include!("../src_1/main.rs");
 }
-fn main() {
-    let req = Request {
-        method: "test",
-        path: "test",
-        protocol: "test",
-        headers: "test",
-        body: "test",
-    };
-    println!("{:?}", std::mem::size_of::<Request>());
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+
+fn main() -> Result<(), Error> {
+    let listener = TcpListener::bind("127.0.0.1:4221")?;
+    let bytes_buffer: Vec<u8> = Vec::with_capacity(512);
+    let bump = Bump::new();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                // dbg!(&stream);
+                let req = Request::from_stream(&stream, &bump)?;
+                let f = match req.path {
+                    "/" => HttpTemplates::Slash.format(""),
+                    _ => HttpTemplates::NotFound.format("")
+                };
+                stream.write_all(f.as_slice())?;
+                stream.flush()?;
             }
             Err(e) => {
-                println!("{}", e);
+                println!("error: {}", e);
             }
         }
     }
-
+    Ok(())
 }
+
+
